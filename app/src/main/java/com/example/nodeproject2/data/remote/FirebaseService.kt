017c885de92cc6715088ad38e0f8133cbb22a8ac
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-07-14 20:15:11 +0900

    feat: fcm 토큰 획득 추가 및 액션바 테마 수정

--- app/src/main/java/com/example/nodeproject2/data/remote/FirebaseService.kt
+++ app/src/main/java/com/example/nodeproject2/data/remote/FirebaseService.kt
@@ -1,22 +1,13 @@
-package com.finpo.app.network
+package com.example.nodeproject2.data.remote
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.os.Build
-import android.util.Log
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.core.os.bundleOf
 import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
+
 
 class FirebaseService : FirebaseMessagingService() {
     private lateinit var notiTitle: String
     private lateinit var notiBody: String
-//    private var startId = R.id.homeFragment
+
+    //    private var startId = R.id.homeFragment
     private var bulletinId: Int = -1
 
     override fun onNewToken(token: String) {
