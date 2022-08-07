5c0e2af3e24bc58961b9799a460c143ec3a2c7bf
yoon <dudwls143@gmail.com>
2022-08-07 18:48:58 +0900

    feat: 홈 레이아웃 변경

--- app/src/main/java/com/example/nodeproject2/ui/home/adapter/HomeAdapter.kt
+++ app/src/main/java/com/example/nodeproject2/ui/home/adapter/HomeAdapter.kt
@@ -14,7 +14,6 @@ import com.example.nodeproject2.widget.utils.Utils
 
 class HomeAdapter(val context: Context, val schedule: List<Schedule>) : RecyclerView.Adapter<HomeAdapter.Holder>() {
 
-    private var selectedPosition = 0
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -30,28 +29,25 @@ class HomeAdapter(val context: Context, val schedule: List<Schedule>) : Recycler
         )
     }
 
-    fun setPageSelected(position: Int) {
-        selectedPosition = position
-    }
+
 
     inner class Holder(val binding: ItemViewPagerHomeBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        @RequiresApi(Build.VERSION_CODES.O)
+//        @RequiresApi(Build.VERSION_CODES.O)
         fun setData(data: Schedule) {
             binding.tvDeadline.text = Utils.getDeadline(data.deadline.toLocalDate())
             binding.data = data
-            if (selectedPosition == adapterPosition) {
-                binding.scheduleItemLayout.setBackgroundResource(R.drawable.bg_gray_color_round_20)
-            }else {
-                binding.scheduleItemLayout.setBackgroundResource(R.drawable.bg_gray_color_round_20)
-            }
+//            if (selectedPosition == adapterPosition) {
+//                binding.scheduleItemLayout.setBackgroundResource(R.drawable.bg_gray_color_round_20)
+//            }else {
+//                binding.scheduleItemLayout.setBackgroundResource(R.drawable.bg_gray_color_round_20)
+//            }
         }
     }
 
     override fun getItemCount(): Int = schedule.size
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: Holder, position: Int) {
         holder.setData(schedule[position])
     }
